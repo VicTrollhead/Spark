e3f23a3163710d4e0d59bc79dbeeb8e67df97401
VicTrollhead <shapovalovtimur16@gmail.com>
2025-02-27 00:02:45 +0100

    spark

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -68,7 +68,7 @@ export default function Welcome() {
                                                 viewBox="0 0 10 11"
                                                 fill="none"
                                                 xmlns="http://www.w3.org/2000/svg"
-                                                className="h-2.5 w-2.5"
+                                                className="h-2.5 w-2.5"flex w-full
                                             >
                                                 <path
                                                     d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001"
