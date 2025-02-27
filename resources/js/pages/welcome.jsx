f2636ddf2efa550eaa81172fbff88bb085afacb8
VicTrollhead <shapovalovtimur16@gmail.com>
2025-02-27 21:42:56 +0100

    fixed boolean error

--- resources/js/pages/welcome.jsx
+++ resources/js/pages/welcome.jsx
@@ -43,7 +43,7 @@ export default function Welcome() {
                                         Read the
                                         <a href="https://laravel.com/docs" target="_blank" className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]">
                                             <span>Documentation</span>
-                                            <svg width={10} height={11} viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-2.5 w-2.5" flex w-full>
+                                            <svg width={10} height={11} viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-2.5 w-2.5 flex w-full" >
                                                 <path d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001" stroke="currentColor" strokeLinecap="square"/>
                                             </svg>
                                         </a>
