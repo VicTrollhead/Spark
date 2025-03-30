b7e9b1610728b212e96b63170b4b2b5a52b3044f
VicTrollhead <shapovalovtimur16@gmail.com>
2025-03-30 12:52:40 +0200

    visual changes, sidebar and header fix

--- resources/js/components/app-extra-sidebar.jsx
+++ resources/js/components/app-extra-sidebar.jsx
@@ -5,10 +5,26 @@ import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, Sid
 import { usePage } from '@inertiajs/react';
 import { BookOpen, Folder, Home, User } from 'lucide-react';
 import { Input } from './ui/input';
+import { useEffect, useState } from 'react';
 
 export function AppExtraSidebar() {
     const { auth } = usePage().props;
     const user = auth?.user;
+    const isLargeScreen = useMediaQuery("(min-width: 1024px)");
+
+    function useMediaQuery(query) {
+        const [matches, setMatches] = useState(window.matchMedia(query).matches);
+
+        useEffect(() => {
+            const mediaQuery = window.matchMedia(query);
+            const handleChange = () => setMatches(mediaQuery.matches);
+
+            mediaQuery.addEventListener("change", handleChange);
+            return () => mediaQuery.removeEventListener("change", handleChange);
+        }, [query]);
+
+        return matches;
+    }
 
 
     const mainNavItems = [
@@ -28,6 +44,7 @@ export function AppExtraSidebar() {
         <Sidebar
             side="right"
             variant="inset"
+            collapsible={isLargeScreen ? "offcanvas" : "none"}
             className="hidden lg:block pr-2.5"
         >
             <SidebarHeader>
