d0a88a075f36034e29cb8a75a02644f7402501ee
VicTrollhead <shapovalovtimur16@gmail.com>
2025-05-04 10:57:40 +0200

    minor changes

--- resources/js/components/user-info.jsx
+++ resources/js/components/user-info.jsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 export function UserInfo({ user, showEmail = false }) {
     const getInitials = useInitials();
-    console.log("User: " + user['name'] + " " + user['is_verified'])
+    console.log('User: ' + user['name'] + ' ' + user['is_verified']);
     return (
         <div className="flex items-center gap-2">
             <Avatar className="h-12 w-12 overflow-hidden rounded-full">
@@ -14,21 +14,16 @@ export function UserInfo({ user, showEmail = false }) {
                 </AvatarFallback>
             </Avatar>
             <div className="grid flex-1 text-left leading-tight">
-                <div className="flex items-center">
-                    <span className="text-lg font-extrabold break-all">{user.name}</span>
+                <div className="flex items-center gap-2">
+                    <span className="truncate text-sm font-normal">@{user.username}</span>
                     {user.is_verified && (
-                        <div className="group relative ml-2">
-                            {/*<span className="absolute -top-7 left-1/2 top -translate-x-1/2 scale-0 transform rounded-md bg-gray-800 px-2 py-1 text-xs text-white opacity-0 transition-all group-hover:scale-100 group-hover:opacity-100">*/}
-                            {/*    Verified*/}
-                            {/*</span>*/}
-                            <span className="flex items-center rounded-lg bg-blue-500 p-0.5 text-xs font-medium text-white">
-                                <Check className="h-3 w-3" />
-                            </span>
-                        </div>
+                        <span className="flex items-center rounded-lg bg-blue-500 p-0.5 text-xs font-medium text-white">
+                            <Check className="h-3 w-3" />
+                        </span>
                     )}
                 </div>
 
-                <span className="truncate text-sm font-normal">@{user.username}</span>
+                <span className="text-lg font-extrabold break-all">{user.name}</span>
                 {showEmail && <span className="text-muted-foreground truncate text-xs">{user.email}</span>}
             </div>
         </div>
