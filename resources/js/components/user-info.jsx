2e4815d0e1abc8285edb2acda1ea0764a21b55f9
VicTrollhead <shapovalovtimur16@gmail.com>
2025-05-18 14:02:39 +0200

    im sorry german x2

--- resources/js/components/user-info.jsx
+++ resources/js/components/user-info.jsx
@@ -1,7 +1,7 @@
 import { Check } from 'lucide-react';
 import { useInitials } from '../hooks/use-initials';
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { getProfileImageUrl } from '../lib/utils';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 // const getProfileImageUrl = (user) => {
 //     const url = user?.profile_image;
@@ -22,25 +22,24 @@ import { getProfileImageUrl } from '../lib/utils';
 export function UserInfo({ user, showEmail = false }) {
     const getInitials = useInitials();
     return (
-        <div className="flex items-center gap-2 min-w-0">
-            <Avatar className="h-12 w-12 overflow-hidden rounded-full flex-shrink-0">
+        <div className="flex min-w-0 items-center gap-2">
+            <Avatar className="h-12 w-12 flex-shrink-0 overflow-hidden rounded-full">
                 <AvatarImage src={getProfileImageUrl(user)} alt={user.name} />
                 <AvatarFallback className="rounded-lg bg-gray-200 text-black dark:bg-gray-700 dark:text-white">
                     {getInitials(user.name)}
                 </AvatarFallback>
             </Avatar>
-            <div className="grid flex-1 min-w-0 text-left leading-tight">
-                <div className="flex items-center gap-1 min-w-0">
-                    <span className="truncate text-[14.5px] text-gray-700 dark:text-neutral-300 max-w-full">@{user.username}</span>
+            <div className="grid min-w-0 flex-1 text-left leading-tight">
+                <div className="flex min-w-0 items-center gap-1">
+                    <span className="max-w-full truncate text-[14.5px] text-gray-700 dark:text-neutral-300">@{user.username}</span>
                     {user.is_verified && (
-                        <span className="flex-shrink-0 flex items-center rounded-lg bg-blue-500 p-0.5 text-xs font-medium text-white">
-          <Check className="h-3 w-3" />
-        </span>
+                        <span className="flex flex-shrink-0 items-center rounded-lg bg-blue-500 p-0.5 text-xs font-medium text-white">
+                            <Check className="h-3 w-3" />
+                        </span>
                     )}
                 </div>
-                <span className="truncate text-lg font-extrabold max-w-full">{user.name}</span>
+                <span className="max-w-full truncate text-lg font-extrabold">{user.name}</span>
             </div>
         </div>
-
     );
 }
